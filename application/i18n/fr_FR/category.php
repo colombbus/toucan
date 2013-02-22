64cd7605433fe96440fe5c4fa1701e8bedc5ba79
bonpra <benoit.prady@colombbus.org>
2013-02-22 19:35:57 +0100

    Addition of categories to organize listing of indicators

---
+++ application/i18n/fr_FR/category.php
@@ -0,0 +1,57 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+ /**
+ * Toucan is a web application to perform evaluation and follow-up of
+ * activities.
+ * Copyright (C) 2010 Colombbus (http://www.colombbus.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$lang=array(
+    "activate_confirm"=>"Si vous confirmez cette action, la catégorie sera utilisée dans Toucan. Êtes-vous sûr ?",
+    "active"=>"Catégorie activée\xc2\xa0:",
+    "create"=>"Créer une nouvelle catégorie",
+    "create_description"=>"Informations sur la nouvelle catégorie :",
+    "create_title"=>"Nouvelle catégorie d'indicateurs",
+    "deactivate_confirm"=>"Êtes-vous sûr de vouloir désactiver la catégorie ? Elle n'apparaîtra plus dans Toucan.",
+    "delete_text"=>"Êtes-vous sûr de vouloir supprimer la catégorie ?",
+    "description"=>"Description\xc2\xa0:",
+    "description_header"=>"Description",
+    "edit_description"=>"Modification des informations de la catégorie\xc2\xa0:",
+    "indicators"=>"Indicateurs",
+    "inherit"=>"Appliquer le même contrôle d'accès que pour l'évaluation\xc2\xa0:",
+    "info"=>"Informations",
+    "main_title"=>"Catégorie d'indicateurs «\xc2\xa0%s\xc2\xa0»",
+    "may_edit"=>"Modifiable par :",
+    "may_view"=>"Consultable par :",
+    "members_description"=>"Indicateurs faisant partie de la catégorie :",
+    "message_activated"=>"Catégorie activée",
+    "message_created"=>"Catégorie créée",
+    "message_deactivated"=>"Catégorie désactivée",
+    "message_deleted"=>"Catégorie supprimée",
+    "message_edited"=>"Catégorie mise à jour",
+    "name"=>"Nom\xc2\xa0:",
+    "name_header"=>"Nom",
+    "no"=>"non",
+    "no_item"=>"Aucune catégorie créée",
+    "owner_description"=>"Définition du propriétaire de la catégorie\xc2\xa0:",
+    "self"=>"Créateur de la catégorie",
+    "set_members"=>"Ajouter / Retirer des indicateurs",
+    "set_members_description"=>"Sélection des indicateurs faisant partie de la catégorie",
+    "set_owner"=>"Modifier le propriétaire",
+    "show_description"=>"Informations sur la catégorie\xc2\xa0:",
+    "view_members"=>"Liste",
+    "yes"=>"oui",
+);
+?>
