147469a0dac410a8ddc09f753067dccf4a764d99
bonpra <benoit.prady@colombbus.org>
2013-03-13 14:41:35 +0100

    Deletion of space character in the beginning of file that broke rtf generation

--- application/controllers/evaluation.php
+++ application/controllers/evaluation.php
@@ -1,4 +1,4 @@
- <?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') or die('No direct script access.');
 /**
  * Toucan is a web application to perform evaluation and follow-up of
  * activities.
