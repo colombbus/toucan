8dbdfb99612504347cf448af0e5d94cc986bf9b7
bonpra <benoit.prady@colombbus.org>
2013-10-03 17:22:22 +0200

    Correction d'un bug dans le cas de l'affichage en accès public

--- application/models/copy.php
+++ application/models/copy.php
@@ -152,11 +152,11 @@ abstract class Copy_Model extends Toucan_Model {
         } else {
             $publicGroup = Group_Model::SPECIAL_GROUP_PUBLIC;
             $registeredGroup = Group_Model::SPECIAL_GROUP_REGISTERED;
+            $publicStates = implode(",", CopyState_Model::getPublishedStates());
             if (isset($user)) {
                 $userGroups = $user->getGroups();
                 if (strlen($userGroups)>0)
                     $userGroups = ",".$userGroups;
-                $publicStates = implode(",", CopyState_Model::getPublishedStates());
                 $query = "select distinct $this->table_name.* from $this->table_name, sessions where (sessions.id = $this->table_name.session_id) and (((sessions.view_id in ($publicGroup, $registeredGroup $userGroups) or sessions.owner_id=$user->id) and ($this->table_name.state_id IN ($publicStates))) or ($this->table_name.owner_id = $user->id))";
             } else {
                 $query = "select distinct $this->table_name.* from $this->table_name, sessions where ((sessions.id = $this->table_name.session_id) and (sessions.view_id=$publicGroup) and ($this->table_name.state_id IN ($publicStates)))";
